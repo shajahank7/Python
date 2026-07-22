17427e8334232e0ced938ec938634c4e91c2e2fd
python <python@email.com>
2026-07-22 21:30:27 +0530

    Add GitHub Actions CI

--- conftest.py
+++ conftest.py
@@ -34,3 +34,16 @@ def login_setup(driver):
 
     # OPTIONAL TEARDOWN
     # login.Verify_Logout()
+
+
+import allure
+
+def after_scenario(context, scenario):
+
+    if scenario.status == "failed":
+
+        allure.attach(
+            context.driver.get_screenshot_as_png(),
+            name="Failure Screenshot",
+            attachment_type=allure.attachment_type.PNG
+        )
