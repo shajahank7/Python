0d4fe15f8fe6e0284bc18c20e9d7a4d56687a56b
python <python@email.com>
2026-07-25 10:50:17 +0530

    Fix Chrome startup for GitHub Actions

--- driver_factory.py
+++ driver_factory.py
@@ -1,11 +1,25 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 class DriverFactory:
+
     @staticmethod
     def get_driver(browser):
+
         if browser == "chrome":
-            return webdriver.Chrome()
+            options = Options()
+            options.add_argument("--headed=new")
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-dev-shm-usage")
+            options.add_argument("--disable-gpu")
+
+            return webdriver.Chrome(options=options)
+
         elif browser == "firefox":
-            return webdriver.Firefox()
+            options = FirefoxOptions()
+            options.add_argument("--headless")
+            return webdriver.Firefox(options=options)
+
         else:
             raise Exception(f"Browser '{browser}' not supported")
