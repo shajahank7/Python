9e5573f87f71a5938bf47d4e17f95d79666b22c0
python <python@email.com>
2026-07-25 11:36:15 +0530

    Fix Chrome headless option

--- driver_factory.py
+++ driver_factory.py
@@ -9,7 +9,7 @@ class DriverFactory:
 
         if browser == "chrome":
             options = Options()
-            options.add_argument("--headed=new")
+            options.add_argument("--headless=new")
             options.add_argument("--no-sandbox")
             options.add_argument("--disable-dev-shm-usage")
             options.add_argument("--disable-gpu")
