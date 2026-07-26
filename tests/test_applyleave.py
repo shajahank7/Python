d0a845c50830dd21f9ec38c7d2e19ec28f4279e7
python <python@email.com>
2026-07-26 11:04:22 +0530

    updated the test_applyleave.py

--- tests/test_applyleave.py
+++ tests/test_applyleave.py
@@ -1,12 +1,7 @@
 import pytest
 from pages.ApplyleavePage import Applyleave
-<<<<<<< HEAD
 
-# @pytest.mark.usefixtures("login_setup")
-=======
-@pytest.mark.skip(reason="Temporarily disabled")
 @pytest.mark.usefixtures("login_setup")
->>>>>>> 8c3529154b601fe88761b2196ea597ad24069bb8
 @pytest.mark.order(1)
 class TestApplyLeaveTest:
     def test_applyleave(self, driver):
